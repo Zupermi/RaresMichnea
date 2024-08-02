69bef165d17209f8e4a0c877d598f1aabe90059e
bmiruna <miruna_bostan@yahoo.com>
2024-08-02 14:24:38 +0300

    add initial structure of the website

---
+++ src/wip/WorkInProgress.js
@@ -0,0 +1,12 @@
+import './wip.css';
+
+const WorkInProgress = () => {
+    return (
+     <div className='container'>
+        <span className="loader"></span>
+        <h1 className='wip-text'>Work in Progress</h1>
+    </div> 
+    )
+}
+ 
+export default WorkInProgress;
