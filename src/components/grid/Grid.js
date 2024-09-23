63f55c2eacd42ac7e09cd48cb6240b3a05d6fb8b
bmiruna <miruna_bostan@yahoo.com>
2024-09-23 21:48:39 +0300

    update hero zone and fix img padding

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,9 +1,8 @@
 import "./grid.css";
-
-const options = ["Videoclipuri Muzicale", "Reclame", "Motion Graphics", "Prezentari Video"]
+import options from "./options";
 
 const Grid = () => {
-    const gridOprions = options.map((option, index) => <div key={index} className="grid-item">{option}</div>)
+    const gridOprions = options.map((option, index) => <div key={index} className="grid-item"><p key={index} >{option.text}</p><img src={option.background} alt={option.text} /></div>)
     return (
         <div className="grid-container">{gridOprions}</div>
     )
--- src/components/grid/options.js
+++ src/components/grid/options.js
@@ -1,6 +1,25 @@
-export default options = [
+import photo1 from "../../assets/photos/photo1.jpg";
+import photo2 from "../../assets/photos/photo2.jpg";
+import photo3 from "../../assets/photos/photo3.jpg";
+import photo4 from "../../assets/photos/photo1.jpg";
+
+const options = [
     {
         text: "Videoclipuri muzicale",
-        background
-    }
-]
+        background: photo1
+    },
+    {
+        text: "Reclame",
+        background: photo2
+    },
+    {
+        text: "Motion Graphics",
+        background: photo3
+    },
+    {
+        text: "Prezentari video",
+        background: photo4
+    },
+];
+
+export default options;
