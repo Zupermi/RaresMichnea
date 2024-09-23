aaf421d0513b884f36b56f293664206350c11ae3
Cristian Crăciun <craciun.cristian.sighet@gmail.com>
2024-09-23 15:31:38 +0300

    create navBar component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import WorkInProgress from './wip/WorkInProgress';
+import NavBar from './nav/NavBar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar></NavBar>
     <WorkInProgress />
   </React.StrictMode>
 );
