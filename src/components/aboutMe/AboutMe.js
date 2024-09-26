4ba0ab57541887e133a665247844c445900be749
bmiruna <miruna_bostan@yahoo.com>
2024-09-27 00:23:59 +0300

    refactor about me section

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -7,34 +7,40 @@ import youtube from "../../assets/photos/youtube.png";
 import linkedIn from "../../assets/photos/linkedIn.png";
 import { useState } from "react";
 
+const socialIcons = [
+    { img: instagram, alt: 'Instagram icon' },
+    { img: mail, alt: 'Email icon' },
+    { img: linkedIn, alt: 'LinkedIn icon' },
+    { img: youtube, alt: 'Youtube icon' }
+  ];
+
 
 const AboutMe = () => {
     const [showHeading, setShowHeading] = useState(true);
-    const aboutMeElements = aboutMeParagrafs.map((info)=>{return(<p className="text-paragraf" >{info.text}</p>)});
-    const misiuneaMeaElements = misouneaMeaParagrafs.map((info)=>{return(<p className="text-paragraf" >{info.text}</p>)});
+    const aboutMeElements = aboutMeParagrafs.map((info, index) => <p key={index} className="text-paragraf" >{info.text}</p>);
+    const misiuneaMeaElements = misouneaMeaParagrafs.map((info, index) => <p key={index} className="text-paragraf" >{info.text}</p>);
+
+    const icons = socialIcons.map((icon, index) => <img key={index} className="icons-wrapper-img" src={icon.img} alt={icon.alt} />);
+
     const showAboutMeElements =(() => {
         setShowHeading(true);
     });
     const showMisiuneaMeaElements =(() => {
         setShowHeading(false);
     });
+
     return (
         <div id="about" className="about-wrapper">
             <img src={aboutImg} alt="About Me" />
             <div className="content-wrapper">
                 <div>
                     <div className="headings">
-                        <h2 className="heading" onClick={showMisiuneaMeaElements}>Misiunea mea</h2>
-                        <h2 className="heading" onClick={showAboutMeElements}>Despre mine</h2>
+                        <h2 key="heading1" className="heading" onClick={showMisiuneaMeaElements}>Misiunea mea</h2>
+                        <h2 key="heading2" className="heading" onClick={showAboutMeElements}>Despre mine</h2>
                     </div>
-                    { showHeading ? <div>{aboutMeElements}</div> : <div>{misiuneaMeaElements}</div>}
+                    { showHeading ? aboutMeElements : misiuneaMeaElements }
                 </div>
-                <div className="icons-wrapper">
-                    <img className="icons-wrapper-img" src={instagram} alt="instagram"></img>
-                    <img className="icons-wrapper-img" src={mail} alt="mail"></img>
-                    <img className="icons-wrapper-img" src={youtube} alt="youtube"></img>
-                    <img className="icons-wrapper-img" src={linkedIn} alt="LinkedIn"></img>
-                </div>
+                <div className="icons-wrapper">{icons}</div>
             </div>
         </div>
     )
